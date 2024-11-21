53d8bf39db9fc139eccc22cbaea460501f5e7713
vladislavabramov <vladislavabramov@trentu.ca>
2024-11-21 12:47:01 -0500

    clean up stray folders

--- database/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod mutation;
-pub mod query;
-
-use sea_orm::{Database, DatabaseConnection};
-
-pub async fn init(db_uri: String) -> DatabaseConnection {
-    Database::connect(db_uri).await.unwrap()
-}
--- utils/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod app_error;
-pub mod jwt;
-pub mod password;
